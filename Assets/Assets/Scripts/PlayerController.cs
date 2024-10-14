8de389d12552b40791e8507dc3181a509a782aed
Tejas Vasishta G R <wasishta38@gmail.com>
2024-10-14 20:54:09 +0530

    Added project and animations
    Added walk and run animations

---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    private float _speedX;
+    private float _speedY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _speedX = Input.GetAxis("Horizontal");
+        Vector3 scale = transform.localScale;
+
+        animator.SetFloat("SpeedX", Mathf.Abs(_speedX));
+        //animator.SetFloat("SpeedY", _speedY);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            animator.SetBool("IsRunning", true);
+        else animator.SetBool("IsRunning", false);
+
+        /*if (Input.GetKey(KeyCode.LeftControl))
+            animator.SetBool("IsCrouching", true);
+        else animator.SetBool("IsCrouching", false);*/
+
+        if (_speedX < 0 && scale.x == 1)
+            scale.x *= -1;
+
+        else if (_speedX > 0 && scale.x == -1)
+            scale.x *= -1;
+
+        transform.localScale = scale;
+    }
+}
